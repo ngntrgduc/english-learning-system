6c4af0c4e39909254ef064054455e01fd8beb02b
ngntrgduc <trungducnguyen1511@gmail.com>
2023-03-27 23:57:00 +0700

    Update code

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import notion
 import cambridge
+import notion
 import thesaurus
 
 from rich import print
@@ -7,20 +7,16 @@ import random
 import time
 
 def split_word(words_list, delimiter=' '):
-    return [word.split(delimiter)[0].lower().strip()
+    return [word.split(delimiter)[0].lower()
             for word in words_list]
 
-raw = """
-VICIOUS | English meaning - Cambridge Dictionary
-INSIDIOUS | English meaning - Cambridge Dictionary
-CONCISE | English meaning - Cambridge Dictionary
-PRAGMATIC | English meaning - Cambridge Dictionary
-INQUISITIVE | English meaning - Cambridge Dictionary
-GENUINE | English meaning - Cambridge Dictionary
-"""
+with open('words.txt', 'r') as file:
+    raw = file.read()
+file.close()
 
-words_list = [word for word in raw.split("\n") 
-              if len(word) > 0]
+words_list = [word.strip() for word in raw.split("\n") 
+              if len(word.strip()) > 0]
+
 words = split_word(words_list)
 # print(words)
 
