dfc507d9466c23a698c4f4d5c1024da3e8efd890
ngntrgduc <trungducnguyen1511@gmail.com>
2024-03-08 14:10:46 +0700

    Use Asynchronous, faster

--- main.py
+++ main.py
@@ -1,26 +1,28 @@
+"""Crawl pronounce of vocab and write to a file to import to Anki"""
+
 import time
 import random
-import cambridge
+import asyncio
+
+import aiofiles
 from tqdm import tqdm
-from rich import print
-from utils.vocabs import read
-from utils.tictoc import timing
 
-def clear_file(file):
-    """Clear vocabs file"""
-    open(file, 'w').close()
-    pass
+import cambridge
+from utils import read, clear_file
+import logger
 
-@timing
-def main():
-    vocabs_file = 'vocabs.txt'
-    vocabs = read(vocabs_file)
-    with open('result.txt', 'w', encoding='utf-8' ) as f:
+async def main():
+    file_name = 'vocabs'
+    tic = time.time()
+    vocabs = read(f'{file_name}.txt')
+    async with aiofiles.open(f'{file_name}.result.txt', 'w', encoding='utf-8') as f:
         for vocab in tqdm(vocabs, desc='Crawl & create', unit='word'):
-            time.sleep(random.random() + 1)
-            pronounce = cambridge.crawl(vocab)
-            f.write(f'{vocab},{pronounce}\n')
-    f.close()
-    clear_file(vocabs_file)
+            await asyncio.sleep(random.uniform(0, 0.5) + 0.5)
+            pronounce = await cambridge.crawl(vocab)
+            await f.write(f'{vocab},{pronounce}\n')
+    
+    clear_file(f'{file_name}.txt')
 
-main()
+    logger.info(f'Crawled {len(vocabs)} vocabs, took {(time.time()-tic)/60:.3f} mins, sleep 0.5-1s')
+
+asyncio.run(main())
