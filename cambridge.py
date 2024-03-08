681b0444bc3d3992f47734c643d046a61fdc55c8
ngntrgduc <trungducnguyen1511@gmail.com>
2024-03-08 14:12:52 +0700

    Use SoupStrainer, Handling when cannot get pronunciation

--- cambridge.py
+++ cambridge.py
@@ -1,8 +1,10 @@
+"""Get pronunciation from Cambridge dictionary"""
+
 import os
-import re
 import requests
 from dotenv import load_dotenv
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, SoupStrainer
+from logger import exception, info
 
 load_dotenv()
 
@@ -10,19 +12,70 @@ headers = {
     'User-Agent': os.getenv('USER_AGENT')
 }
 
-def crawl(vocab):
-    """Crawl and return pronounce of vocabulary from Cambridge dictionary"""
-    
-    url = f'https://dictionary.cambridge.org/dictionary/english/{vocab}'
-    soup = requests.get(url, headers=headers).content
-    page = BeautifulSoup(soup, 'lxml')
+session = requests.Session()
 
-    pronounces = [re.findall('\/.*\/', str(pron.text))[0] for pron in 
-                page.find_all(attrs={'class': 'pron dpron'})]
-    # pronounce = f'UK: {pronounces[0]}\nUS: {pronounces[1]}' // UK and US pronounce
-    try:
-        pronounce = pronounces[0] # just UK pronounce
-    except:
-        print(f'* Cannot get pronounce of {vocab}')
+async def crawl(vocab: str) -> str:
+    """Crawl and return pronunciations of vocab from Cambridge dictionary"""
+
+    base_url = 'https://dictionary.cambridge.org/dictionary/english/'
+    url = base_url + vocab
+    response = session.get(url, headers=headers)
+
+    # Check if Cambridge redirect to home page when the vocab not existed
+    if response.history:
+        if response.history[-1].status_code in range(300, 310):
+            if response.url == base_url:
+                info(f'"{vocab}" does not exist in the Cambridge dictionary')
+                return ''
     
-    return pronounce
+    soup = response.content
+    
+    page = BeautifulSoup(
+        soup, 'lxml',
+        parse_only=SoupStrainer(
+            'div', attrs={'class': 'pos-header dpos-h'}
+        )
+    ).findAll(attrs={'class': 'pos-header dpos-h'})
+
+    pronounce_classes = ('uk dpron-i', 'us dpron-i')
+    pronounces = []
+    pronounce = ''
+
+    for pronounce_class in pronounce_classes:
+        try:
+            pronounce = page[0].find(attrs={'class': pronounce_class})
+            if pronounce:
+                pronounce = pronounce.find(attrs={'class': 'pron dpron'}).text
+            else:
+                pronounce = ''
+        except AttributeError:
+            # Happen when vocab has different form
+            # TODO: still cannot get pronunciations in bellow section
+            pronounce = page[1].find(attrs={'class': pronounce_class})
+            if pronounce:
+                pronounce = pronounce.find(attrs={'class': 'pron dpron'}).text
+            else:
+                pronounce = ''
+        except IndexError:
+            info(f'"{vocab}" does not have pronunciation')
+            return '"UK: ,  US: "'
+        except Exception:
+            exception(f'Cannot get pronounce: {vocab}')
+        finally:
+            pronounces.append(pronounce)
+    
+    return f'"UK: {pronounces[0]},  US: {pronounces[1]}"'
+
+
+if __name__ == '__main__':
+    import time
+    import asyncio
+    tic = time.time()
+    # for vocab in ['tainted', 'religious', 'no doubt', 'vaporizing', 'hii']:
+        # print(vocab, asyncio.run(crawl(vocab)))
+    # vocab = 'hii'
+    # url = f'https://dictionary.cambridge.org/dictionary/english/{vocab}'
+    # res = session.get(url, headers=headers)
+    # print(res.history[0].status_code)
+    # print(res.status_code)
+    print(f'{time.time() - tic}')
