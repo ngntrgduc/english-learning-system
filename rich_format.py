570f80b5cf6a93fd966c74ad24fcf7cc30060db6
ngntrgduc <trungducnguyen1511@gmail.com>
2025-03-08 15:06:56 +0700

    Add rich format

---
+++ rich_format.py
@@ -0,0 +1,7 @@
+def text_blue(text: str) -> str:
+    """Return blue format in rich library."""
+    return f'[blue]{text}[/blue]'
+
+def text_red(text: str) -> str:
+    """Return red format in rich library."""
+    return f'[red]{text}[/red]'
