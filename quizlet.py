57f08a1dc6ec3c75a38b4a83040d5f5f207e0738
ngntrgduc <trungducnguyen1511@gmail.com>
2025-03-10 23:11:22 +0700

    Refactor code

--- quizlet.py
+++ quizlet.py
@@ -1,12 +1,10 @@
-"""Format the export result from Quizlet"""
+"""Format the exported result from Quizlet"""
 
 from utils import read
 
 file_name = 'quizlet'
-vocabs_list = read(f'{file_name}.txt', split=False)
+vocabs_list = read(f'data/{file_name}.txt', delimiter=' - ', split=True)
 
-with open(f'{file_name}.result.txt', 'w', encoding='utf-8') as f:
+with open(f'data/{file_name}.result.txt', 'w', encoding='utf-8') as f:
     for vocab in vocabs_list:
-        f.write(f'{vocab.split(" - ")[0]}\n')
-
-# print(vocabs_list)
+        f.write(f'{vocab}\n')
