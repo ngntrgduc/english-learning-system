57f8dd55864aa2af0e73371a4b5470adb9ca542c
ngntrgduc <trungducnguyen1511@gmail.com>
2025-03-13 00:21:28 +0700

    Add definition, prompt with llm, refactor and restructure code

--- el.py
+++ el.py
@@ -1,12 +1,12 @@
 from random import sample
 import click
 from rich import print
-from rich.console import Console
-from rich.markdown import Markdown
 
 from utils.print import text_blue, text_red, print_vocabs
 from utils.vocab import find_duplicate, is_valid_vocab
 from utils.fzf import fzf
+
+from llm import llm, prompt_command, definition
 
 data_folder = 'data'
 vocabs_file_name = 'vocabs.txt'
@@ -28,6 +28,7 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 def main(ctx):
     ctx.ensure_object(dict)
     ctx.obj.setdefault('data', data)
+    ctx.obj.setdefault('original_length', vocab_length)
     ctx.obj.setdefault('print', False)
 
 def check_valid_range(number: int) -> None:
@@ -149,56 +150,9 @@ def print_to_console(obj, result, *args, **kwargs) -> None:
     if obj['print']:
         print_vocabs(obj['data'])
 
-@main.command()
-@click.pass_obj
-def llm(obj) -> None:
-    """Cenerate sentences/paragraphs with given vocabs using Gemini."""
-    if len(obj['data']) == vocab_length:
-        print('No vocab were given.')
-        return
-
-    if obj['print']:
-        print_vocabs(obj['data'])
-        obj['print'] = False
-
-    if len(obj['data']) > 5:
-        print(f"{text_red('The number of vocab is large (>5)')}, would you want to continue? [y/N]:")
-        answer = input()
-        if answer.lower() == 'n':
-            return
-        if answer.lower() != 'y':
-            return
-
-    from google import genai
-    import os
-    from dotenv import load_dotenv
-    load_dotenv()
-    GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
-
-    client = genai.Client(api_key=GEMINI_API_KEY)
-    response = client.models.generate_content(
-        model='gemini-2.0-flash', 
-        contents=f"""{obj['data']}
-
-        With this given list of vocabulary, provide with grammartically correct: 
-        - the definition with its word form in parentheses ()
-        - 3 sentences using it
-    
-        Also wrap the given vocabulary in the result with square bracket, bold and italic format (***). 
-        Remember not to response with irrelevant information.
-        """
-    )
-    console = Console(width=90)
-    console.rule()
-    console.print(Markdown(response.text))
-    console.rule()
-
-def chat() -> None:
-    # I want you to act like Ludwig.guru site to help me write better English. 
-    # Provide me contextual examples of how words and phrases are used with reliable sources from now on. 
-    # https://ai.google.dev/gemini-api/docs/text-generation?lang=python#chat
-    return
-
-
 if __name__ == '__main__':
+    # LLM commands
+    main.add_command(llm)
+    main.add_command(prompt_command)
+    main.add_command(definition)
     main()
---
+++ llm.py
@@ -0,0 +1,91 @@
+import os
+import click
+from dotenv import load_dotenv
+from google import genai
+from rich import print as rprint
+
+from utils.print import print_vocabs, print_console, text_red
+
+load_dotenv()
+GEMINI_API_KEY = os.getenv('GEMINI_API_KEY')
+model = 'gemini-2.0-flash'
+client = genai.Client(api_key=GEMINI_API_KEY)
+
+def is_valid_obj(obj: dict) -> bool:
+    """Check before perform API calling."""
+    if obj['print']:
+        print_vocabs(obj['data'])
+        obj['print'] = False
+    
+    if len(obj['data']) == obj['original_length']:
+        print('No vocabs were given.')
+        return False
+
+    return True
+
+@click.command()
+@click.pass_obj
+def llm(obj) -> None:
+    """Definition, word form, and sentences with given vocabs."""
+    if not is_valid_obj(obj):
+        return
+    
+    if len(obj['data']) > 5:
+        rprint(text_red('The number of vocab is large (>5) for this command, '), end='')
+        print('continue? [y/N]:')
+        answer = input()
+        if answer.lower() == 'n':
+            return
+        if answer.lower() != 'y':
+            return
+    
+    response = client.models.generate_content(
+        model=model, 
+        contents=f"""{obj['data']}
+
+        With this given list of vocabulary, provide with grammartically correct: 
+        - the definition with its word form in parentheses ()
+        - 3 sentences using it
+    
+        Also wrap the given vocabulary in the result with square bracket, bold and italic format (***). 
+        Remember not to response with irrelevant information.
+        """
+    )
+    print_console(response.text)
+
+@click.command('prompt')
+@click.argument('prompt', type=str, default='')
+@click.pass_obj
+def prompt_command(obj, prompt) -> None:
+    """Prompt whatever you want"""
+    if not prompt:
+        prompt = input('user> ')
+
+    if obj['print']:
+        print_vocabs(obj['data'])
+        obj['print'] = False
+
+    if len(obj['data']) == obj['original_length']:
+        content = prompt
+    else:
+        content = f'{obj['data']}\n{prompt}'
+
+    response = client.models.generate_content(
+        model=model, 
+        contents=content
+    )
+    print_console(response.text)
+
+@click.command()
+@click.pass_obj
+def definition(obj) -> None:
+    """Get definition of vocab."""
+    if not is_valid_obj(obj):
+        return
+
+    response = client.models.generate_content(
+        model=model, 
+        contents=f"""{obj['data']},
+        With given vocabs, provide English definition and (word form)"""
+    )
+    print_console(response.text)
