d2a8fdfc21064c53e0bcaf4d3483c1a2a79ae931
ngntrgduc <trungducnguyen1511@gmail.com>
2025-03-22 13:35:41 +0700

    Handle multiple flags for different files, use object instead global variables

--- el.py
+++ el.py
@@ -2,15 +2,6 @@ import click
 from rich import print
 
 
-data_folder = 'data'
-# vocabs_file_name = 'vocabs.txt'
-vocabs_file_name = 'phrasal_verb.txt'
-vocabs_file_path = f'{data_folder}/{vocabs_file_name}'
-with open(vocabs_file_path, 'r', encoding='utf-8') as f:
-    data = f.read().splitlines()
-
-vocab_length = len(data)
-
 # changes the default parameters to -h and --help instead of just --help
 CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
 @click.group(
@@ -20,11 +11,30 @@ CONTEXT_SETTINGS = dict(help_option_names=['-h', '--help'])
     chain=True,
 )
 @click.pass_context
-def main(ctx):
+@click.option('-i', '--idiom', help='Work with idioms file.', is_flag=True, default=False)
+@click.option('-p', '--phrasal-verb', help='Work with phrasal verbs file.', is_flag=True, default=False)
+def main(ctx, idiom, phrasal_verb):
+    from utils.file import read_data
+
+    if idiom and phrasal_verb:
+        raise ValueError('Cannot works with multiple files.')
+    
+    if idiom:
+        file_name = 'idiom.txt'
+    elif phrasal_verb:
+        file_name = 'phrasal_verb.txt'
+    else:
+        file_name = 'vocabs.txt'
+
+    file_path = f'data/{file_name}'
+    # print(file_path)
+    data = read_data(file_path)
+    
     ctx.ensure_object(dict)
     ctx.obj.setdefault('data', data)
-    ctx.obj.setdefault('original_length', vocab_length)
+    ctx.obj.setdefault('original_length', len(data))
     ctx.obj.setdefault('print', False)
+    ctx.obj.setdefault('file_path', file_path)
 
 @main.command()
 @click.argument('number', type=click.IntRange(1,100), default=5)
@@ -37,22 +47,24 @@ def random(ctx, number: int) -> None:
     ctx.obj['print'] = True
 
 @main.command()
-def check() -> None:
+@click.pass_obj
+def check(obj) -> None:
     """Check vocab file."""
     from utils.vocab import find_duplicate
     from utils.print import text_red
 
     print('Checking duplicated vocabs...')
-    print(duplicated if (duplicated := find_duplicate(data)) else [])
+    print(duplicated if (duplicated := find_duplicate(obj['data'])) else [])
 
     print('Checking invalid vocabs...')
-    for vocab in data:
+    for vocab in obj['data']:
         if vocab[0].isupper():
             print(f' - {text_red(vocab)}')
  
 @main.command()
 @click.argument('vocab', type=str)
-def add(vocab: str) -> None:
+@click.pass_obj
+def add(obj, vocab: str) -> None:
     """Add a vocab to file."""
     from utils.vocab import is_valid_vocab
     from utils.print import text_blue
@@ -60,32 +72,32 @@ def add(vocab: str) -> None:
     if not is_valid_vocab(vocab):
         return
 
-    if vocab in data:
-        print(f' - {text_blue(vocab)} already exists in {vocabs_file_path}.')
+    if vocab in obj['data']:
+        print(f' - {text_blue(vocab)} already exists in {obj['file_path']}.')
         if not click.confirm('Do you want to update it?', default=True, show_default=True):
             return
         else:
-            with open(vocabs_file_path, 'w', encoding='utf-8') as f:
-                for v in data:
+            with open(obj['file_path'], 'w', encoding='utf-8') as f:
+                for v in obj['data']:
                     if v == vocab:
                         continue
 
                     f.write(f'{v}\n')
 
                 f.write(f'{vocab}\n')
-            print(f' - Updated {text_blue(vocab)} in {vocabs_file_path}.')
+            print(f' - Updated {text_blue(vocab)} in {obj['file_path']}.')
             return
-            
     
     vocab = vocab.strip().lower()
-    with open(vocabs_file_path, 'a', encoding='utf-8') as f:
+    with open(obj['file_path'], 'a', encoding='utf-8') as f:
         f.write(f'{vocab}\n')
-        print(f' - Added {text_blue(vocab)} to {vocabs_file_path}.')
+        print(f' - Added {text_blue(vocab)} to {obj['file_path']}.')
 
 @main.command()
-def stat() -> None:
+@click.pass_obj
+def stat(obj) -> None:
     """Show vocab statistics."""
-    print(f' - Total vocabs: {vocab_length}')
+    print(f' - Total vocabs: {obj['original_length']}')
 
 @main.command()
 @click.argument('character', type=str)
@@ -97,7 +109,7 @@ def start(ctx, character: str) -> None:
     assert character.isalpha(), 'Character must be in the alphabet.'
 
     character = character.lower()
-    list_vocabs = [vocab for vocab in data if vocab.lower().startswith(character)]
+    list_vocabs = [vocab for vocab in ctx.obj['data'] if vocab.lower().startswith(character)]
     if not list_vocabs:
         print(f' - There are no vocabs started with {text_blue(character)}.')
     else:
@@ -105,26 +117,28 @@ def start(ctx, character: str) -> None:
         ctx.obj['data'] = sorted(list_vocabs)
 
 @main.command()
-def delete() -> None:
+@click.pass_obj
+def delete(obj) -> None:
     """Delete a vocab using fzf."""
     from utils.fzf import fzf
 
     def _delete():
-        for vocab in data:
+        for vocab in obj['data']:
             print(vocab, flush=True)
     
     selected_vocab = fzf(_delete)
     if not selected_vocab:
         return
 
-    with open(vocabs_file_path, 'w', encoding='utf-8') as f:
-        for vocab in data:
+    print(obj['file_path'])
+    with open(obj['file_path'], 'w', encoding='utf-8') as f:
+        for vocab in obj['data']:
             if selected_vocab == vocab:
                 continue
 
             f.write(f'{vocab}\n')
 
-        print(f" - Removed {click.style(selected_vocab, fg='blue')} in {vocabs_file_path}.")
+        print(f" - Removed '{selected_vocab}' in {obj['file_path']}.")
 
 @main.command()
 @click.pass_context
@@ -133,7 +147,7 @@ def find(ctx) -> None:
     from utils.fzf import fzf
 
     def _find():
-        for vocab in data:
+        for vocab in ctx.obj['data']:
             print(vocab, flush=True)
 
     vocab = fzf(_find)
@@ -159,10 +173,11 @@ def tail(ctx, number: int) -> None:
     ctx.obj['print'] = True
 
 @main.command()
-def all() -> None:
+@click.pass_obj
+def all(obj) -> None:
     """Display all vocabs."""
     from utils.print import print_vocabs
-    print_vocabs(data)
+    print_vocabs(obj['data'])
 
 @main.result_callback()
 @click.pass_obj
